2d1ead7d4b3c1b472a3c11c8ea4c41d4bb471aa7
Guillermo Casanova <Casanova@MacBook-Pro.local>
2022-06-10 18:24:25 -0400

    + Adding styling to create account page

--- assets/global.js
+++ assets/global.js
@@ -1203,3 +1203,59 @@ customElements.define('variant-radios', VariantRadios);
 //   document.querySelector('variant-radios').setUpEvents(); 
 // }); 
 
+
+
+
+
+
+
+class CustomInputWrapper extends HTMLElement {
+  constructor() {
+    super(); 
+    this.init(); 
+  }
+
+  init() {
+
+    let textFieldWrapper = this; 
+
+    console.log(this);
+    
+    if(this.querySelector('input') !== null) {
+
+      this.querySelector('input').addEventListener('change', function(event) {
+          let input = event.currentTarget; 
+
+          console.log('change');
+          if(input.value.length > 0) {
+              textFieldWrapper.classList.add('is-active'); 
+          } else {
+              textFieldWrapper.classList.remove('is-active'); 
+          }
+
+      }); 
+    
+    let changeEvent = new Event("change"); 
+    
+    this.querySelector('input').dispatchEvent(changeEvent); 
+    
+    this.querySelector('input').addEventListener('focus', function() {
+          textFieldWrapper.classList.add('is-active'); 
+    }); 
+    
+    
+    this.querySelector('input').addEventListener('blur', function(event) {
+        let input = event.currentTarget; 
+          if(input.value.length > 0) {
+              textFieldWrapper.classList.add('is-active'); 
+          } else {
+              textFieldWrapper.classList.remove('is-active'); 
+          }
+    }); 
+}
+  }
+
+}
+
+customElements.define('custom-input-wrapper', CustomInputWrapper);
+
