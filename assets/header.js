1497110bbb3bd8a95a6009e01486cd19a62570dd
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2023-07-28 16:57:47 -0400

    = Debuggin throttle

--- assets/header.js
+++ assets/header.js
@@ -271,26 +271,26 @@ class AnnouncementBar extends HTMLElement {
   init() {
     this.applyMargin(); 
 
-    // Throttle function to limit the rate at which the function to apply margin is called
-    function throttle(func, delay) {
-      let timeoutId;
-      return function() {
-        const context = this;
-        const args = arguments;
-        if (!timeoutId) {
-          timeoutId = setTimeout(function() {
-            func.apply(context, args);
-            timeoutId = null;
-          }, delay);
-        }
-      };
-    }
+    // // Throttle function to limit the rate at which the function to apply margin is called
+    // function throttle(func, delay) {
+    //   let timeoutId;
+    //   return function() {
+    //     const context = this;
+    //     const args = arguments;
+    //     if (!timeoutId) {
+    //       timeoutId = setTimeout(function() {
+    //         func.apply(context, args);
+    //         timeoutId = null;
+    //       }, delay);
+    //     }
+    //   };
+    // }
 
-    // Throttle the onResize function with a delay of 250ms
-    const throttledResize = throttle(this.applyMargin, 250);
+    // // Throttle the onResize function with a delay of 250ms
+    // const throttledResize = throttle(this.applyMargin, 250);
 
-    // Attach the throttled function to the window resize event
-    window.addEventListener('resize', throttledResize);
+    // // Attach the throttled function to the window resize event
+    // window.addEventListener('resize', throttledResize);
   }
   applyMargin() {
       //Gives main and the off canvas cart a margin top to make up for the header covering the page when the announcement bar is active
